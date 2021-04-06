24372c678488f9251da2c35a2e5345ae451395bf
Dennis <dmarlo96@live.ca>
2021-04-06 18:16:53 -0400

    Ingredients Validation

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,12 +6,21 @@ import Ingredient from './Ingredient/Ingredient';
 const burger = (props) => {
 
     // transform ingredients object to array
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(ingKey => {
             return [...Array(props.ingredients[ingKey])].map((_, i) => {
                 return <Ingredient key={ingKey + i} type={ingKey} />;
             });
-        });
+        })
+        // allows us to transform an array into something else
+        // in this case we want to flatten the array
+        .reduce((prevValue, currValue) => {
+            return prevValue.concat(currValue)
+        }, []); 
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
 
     return (
         <div className={classes.Burger}>
