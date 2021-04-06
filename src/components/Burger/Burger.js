1dc84c24831c4dd07640d20a70cf2cae79230ccc
Dennis <dmarlo96@live.ca>
2021-04-06 18:06:18 -0400

    Dynamic Burger Component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,19 @@ import classes from './Burger.module.css';
 import Ingredient from './Ingredient/Ingredient';
 
 const burger = (props) => {
+
+    // transform ingredients object to array
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(ingKey => {
+            return [...Array(props.ingredients[ingKey])].map((_, i) => {
+                return <Ingredient key={ingKey + i} type={ingKey} />;
+            });
+        });
+
     return (
         <div className={classes.Burger}>
             <Ingredient type="bread-top"/>
-            <Ingredient type="cheese"/>
-            <Ingredient type="meat"/>
+            {transformedIngredients}
             <Ingredient type="bread-bottom"/>
         </div>
     );
