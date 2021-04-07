977eee0c8ed74aed5ff5e1cb8488893bf5d9bc39
Dennis <dmarlo96@live.ca>
2021-04-07 19:52:54 -0400

    Firebase Post Request with Spinner

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -8,7 +8,8 @@ class Modal extends Component {
 
     // stop modal/ordersummary from re-rendering after ingredient button clicks
     shouldComponentUpdate(nextProps, nextState) {
-        return nextProps.show !== this.props.show;
+        return nextProps.show !== this.props.show || 
+               nextProps.children !== this.props.children;
     }
 
     componentDidUpdate() {
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import axios from '../../axios-orders';
 
 import Auxillary from '../../hoc/Auxillary/Auxillary';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -29,7 +31,8 @@ class BurgerBuilder extends Component {
         },
         totalPrice: 3.50,
         purchasable: false,
-        purchasing: false
+        purchasing: false,
+        loading: false
     };
 
     // disable order button by checking ingredient count
@@ -108,20 +111,51 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        // alert('You continued!')
+        this.setState({loading: true});
+        const order = {
+            ingredients: this.state.ingredients,
+            // recalculate the price on the server
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Dennis Martinez',
+                phoneNumber: '1234567890',
+                email: 'test@test.com',
+                address: {
+                    street: '123 Main Street',
+                    postalCode: 'NT6123',
+                    city: 'Toronto',
+                    country: 'Canada'
+                }
+            },
+            deliveryMethod: 'Delivery'
+        };
+
+        axios.post('/orders.json', order)
+            .then(response => {
+                this.setState({loading: false, purchasing: false});
+            })
+            .catch(error => {
+                this.setState({loading: false, purchasing: false});
+            });
     }
 
     render() {
+        let orderSummary = <OrderSummary 
+                ingredients={this.state.ingredients}
+                price={this.state.totalPrice}
+                purchasedCancelled={this.purchaseCancelHandler}
+                purchasedContinued={this.purchaseContinueHandler}/>;
+
+        if (this.state.loading) {
+            orderSummary = <Spinner />
+        }
+
         return (
             <Auxillary>
                 <Modal 
                     show={this.state.purchasing} 
                     modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary 
-                        ingredients={this.state.ingredients}
-                        price={this.state.totalPrice}
-                        purchasedCancelled={this.purchaseCancelHandler}
-                        purchasedContinued={this.purchaseContinueHandler}/>
+                    {orderSummary}
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
