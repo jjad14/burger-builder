83d2c7123d5a2fdc1338f405f8600039b15ed9a8
Dennis <dmarlo96@live.ca>
2021-04-09 23:50:04 -0400

    Navigation Links

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import classes from './Order.module.css';
 
-const order = (props) = (
+const order = (props) => (
     <div className={classes.Order}>
         <p>Ingredients: Salad (1)</p>
         <p>Price: <string>CAD 7.00</string></p>
