1151537ad23f121991bd8afaa3b0d60cd4a9f970
Dennis <dmarlo96@live.ca>
2021-04-12 21:01:53 -0400

    Redux Setup

---
+++ src/store/reducer.js
@@ -0,0 +1,14 @@
+import * as actionTypes from './actions';
+
+// define state
+const initialState = {
+    ingredients: null,
+    totalPrice: 3.50
+};
+
+// create reducer
+const reducer = (state = initialState, action) => {
+    
+};
+
+export default reducer;
