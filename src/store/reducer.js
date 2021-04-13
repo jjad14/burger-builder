ce7f7d592bf38ca92be05bc77b2dd8bf61bc0dcc
Dennis <dmarlo96@live.ca>
2021-04-12 21:11:55 -0400

    Reducer for Ingredients

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,13 +2,39 @@ import * as actionTypes from './actions';
 
 // define state
 const initialState = {
-    ingredients: null,
+    ingredients: {
+        salad: 0,
+        meat: 0,
+        cheese: 0,
+        bacon: 0,
+    },
     totalPrice: 3.50
 };
 
 // create reducer
 const reducer = (state = initialState, action) => {
-    
+    switch (action.type) {
+        case actionTypes.ADD_INGREDIENT:
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    // ES6 feature, get name of ingredient and overwrite value for it, dyanamically
+                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+                }
+            };
+        case actionTypes.REMOVE_INGREDIENT:
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    // ES6 feature, get name of ingredient and overwrite value for it, dyanamically
+                    [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+                }
+            };
+        default:
+            return state;
+    }
 };
 
 export default reducer;
